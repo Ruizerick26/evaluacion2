1c8e53b88ec13daccf8a4c0320cf66ad47204a2c
Ruizerick26 <ruizerick2525@gmail.com>
2023-08-04 12:16:46 -0500

    todo funcional

--- src/registros.java
+++ src/registros.java
@@ -1,2 +1,204 @@
-package PACKAGE_NAME;public class registros {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class registros {
+    private JPanel root;
+    private JTextField codigotxt;
+    private JTextField cedulatxt;
+    private JTextField nombretxt;
+    private JTextField fechatxt;
+    private JComboBox signotxt;
+    private JButton buscarCBt;
+    private JButton buscarNbt;
+    private JButton buscarSbt;
+    private JButton borrarbt;
+    private JButton actualizarbt;
+    private JButton ingresarbt;
+    private JButton limpiarButton;
+
+    static final String db_URL="jdbc:mysql://localhost/POO3";
+    static final String usser="root";
+    static final String pass="root_bas3";
+    static final String query="SELECT * from REGISTROS";
+    private boolean data = false;
+
+    registrosC persona = new registrosC();
+    public registros(){
+        ingresarbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                persona.setCodigo(codigotxt.getText());
+                persona.setCedula(cedulatxt.getText());
+                persona.setNombre(nombretxt.getText());
+                persona.setFecha(fechatxt.getText());
+                persona.setSigno((String) signotxt.getSelectedItem());
+
+                try(
+                        Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+                        Statement stan= conn.createStatement();
+                        ){
+                            String queryI = "Insert into REGISTROS VALUES ('" + persona.getCodigo() + "','"+ persona.getCedula() + "','" + persona.getNombre() + "','" + persona.getFecha() + "','"+persona.getSigno() + "')";
+                            System.out.println(queryI);
+                            stan.executeUpdate(queryI);
+                            JOptionPane.showMessageDialog(null,"USUARIO REGISTRADO","Regitros",JOptionPane.INFORMATION_MESSAGE);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        limpiarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                codigotxt.setText("");
+                cedulatxt.setText("");
+                nombretxt.setText("");
+                fechatxt.setText("");
+                signotxt.setSelectedItem("");
+            }
+        });
+        actualizarbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setCodigo(codigotxt.getText());
+                persona.setCedula(cedulatxt.getText());
+                persona.setNombre(nombretxt.getText());
+                persona.setFecha(fechatxt.getText());
+                persona.setSigno((String) signotxt.getSelectedItem());
+                try(
+                        Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+                        Statement stat = conn.createStatement();
+                        ){
+                            String queryI = "UPDATE REGISTROS SET cedula = '"+ persona.getCedula() + "', nombre = '" + persona.getNombre() + "', fecha = '" + persona.getFecha() + "', signo = '"+persona.getSigno() + "' " +
+                                    "WHERE codigo = '"+ persona.getCodigo() + "'";
+                            stat.executeUpdate(queryI);
+                            System.out.println(queryI);
+                            JOptionPane.showMessageDialog(null,"USUARIO ACTUALIZADO","Regitros",JOptionPane.INFORMATION_MESSAGE);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
+        borrarbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setCodigo(codigotxt.getText());
+                try(
+                        Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+                        Statement stat = conn.createStatement();
+                        ){
+                            String queriI = "DELETE FROM REGISTROS WHERE codigo = '" + persona.getCodigo() + "'";
+                            stat.executeUpdate(queriI);
+                            System.out.println(queriI);
+                            JOptionPane.showMessageDialog(null,"USUARIO ELIMINADO","Regitros",JOptionPane.INFORMATION_MESSAGE);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        buscarCBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setCodigo(codigotxt.getText());
+                try(
+                        Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+                        Statement stat = conn.createStatement();
+                        ResultSet rc = stat.executeQuery(query);
+                        ){
+                            while(rc.next()) {
+                                if (persona.getCodigo().equals(rc.getString("codigo"))) {
+                                    nombretxt.setText(rc.getString("nombre"));
+                                    cedulatxt.setText(rc.getString("cedula"));
+                                    fechatxt.setText(rc.getString("fecha"));
+                                    signotxt.setSelectedItem(rc.getString("signo"));
+                                    data = false;
+                                    break;
+                                }
+                                else{
+                                    data = true;
+                                }
+                            }
+                            if(data == true){
+                                JOptionPane.showMessageDialog(null,"USUARIO NO ENCONTRADO","Regitros",JOptionPane.ERROR_MESSAGE);
+                            }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        buscarNbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setNombre(nombretxt.getText());
+                try(
+                        Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+                        Statement stat = conn.createStatement();
+                        ResultSet rc = stat.executeQuery(query);
+                ){
+                    while(rc.next()) {
+                        if (persona.getNombre().equals(rc.getString("nombre"))) {
+                            codigotxt.setText(rc.getString("codigo"));
+                            cedulatxt.setText(rc.getString("cedula"));
+                            fechatxt.setText(rc.getString("fecha"));
+                            signotxt.setSelectedItem(rc.getString("signo"));
+                            data = false;
+                            break;
+                        }
+                        else{
+                            data = true;
+                        }
+                    }
+                    if(data == true){
+                        JOptionPane.showMessageDialog(null,"USUARIO NO ENCONTRADO","Regitros",JOptionPane.ERROR_MESSAGE);
+                    }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        buscarSbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setSigno((String) signotxt.getSelectedItem());
+                try(
+                        Connection conn = DriverManager.getConnection(db_URL,usser,pass);
+                        Statement stat = conn.createStatement();
+                        ResultSet rc = stat.executeQuery(query);
+                ){
+                    while(rc.next()) {
+                        if (persona.getSigno().equals(rc.getString("signo"))) {
+                            codigotxt.setText(rc.getString("codigo"));
+                            cedulatxt.setText(rc.getString("cedula"));
+                            fechatxt.setText(rc.getString("fecha"));
+                            nombretxt.setText(rc.getString("nombre"));
+                            data = false;
+                            break;
+                        }
+                        else{
+                            data = true;
+                        }
+                    }
+                    if(data == true){
+                        JOptionPane.showMessageDialog(null,"USUARIO NO ENCONTRADO","Regitros",JOptionPane.ERROR_MESSAGE);
+                    }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("registros");
+        frame.setContentPane(new registros().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- src/registrosC.java
+++ src/registrosC.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class registrosC {
+public class registrosC {
+    private String codigo;
+    private String cedula;
+    private String nombre;
+
+    private String fecha;
+    private String signo;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getSigno() {
+        return signo;
+    }
+
+    public void setSigno(String signo) {
+        this.signo = signo;
+    }
+
+    public registrosC(String codigo, String cedula, String nombre, String fecha, String signo) {
+        this.codigo = codigo;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.signo = signo;
+    }
+
+    public registrosC() {
+    }
 }
